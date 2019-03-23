924d214091dc42988d5d1cc94d019a4728b58371
assaf <asco88@gmail.com>
2019-03-23 21:42:16 +0200

    complete initial commit

--- src/flow.js
+++
@@ -1,8 +0,0 @@
-const { Step, Workflow } = require('./classes');
-
-const step = new Step('firstStep');
-const workflow = new Workflow(new Step('second', () => {
-    console.log('here1');
-}));
-
-workflow.startWorkflow();
--- src/index.js
+++ src/index.js
@@ -1,10 +1,68 @@
-const { Step, Workflow } = require('./classes');
+var today = new Date();
 
-const step = new Step('firstStep');
-const workflow = new Workflow([new Step('second', (data) => {
-    console.log(data.variables.test);
-    data.errors['test2'] = 'some funky error!';
-})]);
 
-workflow.setInitialData({'other': 'wow'});
-workflow.startWorkflow();
+class Step {
+    constructor(name, action) {
+        this.name = name;
+        this.action = action;
+    }
+
+    performAction(data, done) {
+        this.action(data, done);
+    }
+}
+
+class Workflow {
+    constructor(steps, initialData) {
+        this.steps = steps;
+        this.data = {
+            variables: {},
+            errors: {}
+        }
+        this.pos = 0;
+        this.errorHandler = () => {
+            console.log('this is some default error handler');
+        }
+
+        if (initialData) {
+            this.setInitialData(initialData);
+        }
+    }
+
+    setInitialData(data) {
+        this.data.variables = {...this.data.variables, ...data};
+    }
+
+    addStep(step) {
+        this.steps.push(step);
+    }
+
+    setErrorHandler(handler) {
+        this.errorHandler = handler;
+    }
+
+    startWorkflow() {
+        this.startNextStep();
+    }
+
+    startNextStep() {
+        var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+        var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+        var dateTime = date+' '+time;
+
+        console.log(`[${dateTime} | step: ${this.steps[this.pos].name}]`);
+        this.steps[this.pos].performAction(this.data, (nextPos) => {
+            if (Object.keys(this.data.errors).length === 0) {
+                this.pos = nextPos || this.pos + 1;
+                this.startNextStep();
+            } else {
+                console.log(`[${dateTime} | error]`);
+                this.errorHandler(this.data);
+            }
+        });
+    }
+}
+
+module.exports = {
+    Step, Workflow
+}
